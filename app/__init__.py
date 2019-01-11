67a06e6cb6a93829133aa7fd2b7778c7b6f5bab1
Alan Lau <alanlau97@gmail.com>
2019-01-10 22:00:54 -0500

    logging to file

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,8 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 import logging
 from logging.handlers import SMTPHandler
+from logging.handlers import RotatingFileHandler
+import os
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -30,6 +32,18 @@ if not app.debug:
             credentials=auth, secure=secure)
         mail_handler.setLevel(logging.ERROR) # only logging error not warning
         app.logger.addHandler(mail_handler)
+    # logging to file
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
+                                       backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
 
 # testing email:
 '''
@@ -52,3 +66,4 @@ export MAIL_PASSWORD=<your-gmail-password>
 use less secure gmail
 https://support.google.com/accounts/answer/6010255?hl=en
 '''
+# logging categories: DEBUG, INFO, WARNING, ERROR, CRITICAL
