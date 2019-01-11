126ffbea574a28a224352be8353de7fb2de518fe
Alan Lau <alanlau97@gmail.com>
2019-01-10 21:40:56 -0500

    configured email to report error

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,8 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+import logging
+from logging.handlers import SMTPHandler
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -12,3 +14,41 @@ login = LoginManager(app)
 login.login_view = 'login'
 
 from app import routes, models, errors
+
+if not app.debug:
+    if app.config['MAIL_SERVER']:
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+            toaddrs=app.config['ADMINS'], subject='Microblog Failure',
+            credentials=auth, secure=secure)
+        mail_handler.setLevel(logging.ERROR) # only logging error not warning
+        app.logger.addHandler(mail_handler)
+
+# testing email:
+'''
+use a fake server SMTP debugging server from python
+open another terminal
+python -m smtpd -n -c DebuggingServer localhost:8025
+go to first terminal
+export MAIL_SERVER=localhost
+export MAIL_PORT=8025
+ps: use set for windows
+'''
+# second approach
+'''
+configure a real email server
+export MAIL_SERVER=smtp.googlemail.com
+export MAIL_PORT=587
+export MAIL_USE_TLS=1
+export MAIL_USERNAME=<your-gmail-username>
+export MAIL_PASSWORD=<your-gmail-password>
+use less secure gmail
+https://support.google.com/accounts/answer/6010255?hl=en
+'''
