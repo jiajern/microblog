fe4f9950c429fdae7cd858bc20ad075ac1b3dc28
Alan Lau <alanlau97@gmail.com>
2019-01-16 23:57:29 -0500

    small bug

--- app/models.py
+++ app/models.py
@@ -50,7 +50,7 @@ class User(UserMixin, db.Model):
         return self.followed.filter(
             followers.c.followed_id == user.id).count() > 0
 
-     def followed_posts(self):
+    def followed_posts(self):
         followed = Post.query.join(
             followers, (followers.c.followed_id == Post.user_id)).filter(
                 followers.c.follower_id == self.id)
